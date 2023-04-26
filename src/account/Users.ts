289d004c1e971433d1f729c94a9ff4303ad45109
Juan Laquiz <juanilaquiz@gmail.com>
2023-04-26 11:52:13 -0300

    Keep migrating libs

--- src/account/Users.ts
+++ src/account/Users.ts
@@ -20,6 +20,7 @@ export interface User {
     is_manager: boolean;
   };
   picture: string;
+  // cspell:disable-next-line
   permissions: ('splightadmin' | 'admin' | 'editor')[];
   roles: string[];
 }
--- src/engine/SetPoints.ts
+++ src/engine/SetPoints.ts
@@ -2,6 +2,7 @@ import { BaseRestClient } from '../rest/BaseRestClient.js';
 import { Asset, Attribute, Headers } from '../types.js';
 import { Path } from '../Urls.js';
 
+// cspell:disable-next-line
 export interface Setpoint {
   id: string;
   type: string;
@@ -12,7 +13,9 @@ export interface Setpoint {
 }
 
 export const SetPointsClient = (headers: Headers) => {
+  // cspell:disable-next-line
   const basePath = Path('v2/engine/setpoints/');
+  // cspell:disable-next-line
   const baseClient = BaseRestClient<Setpoint, Setpoint>(basePath, headers);
   return baseClient;
 };
