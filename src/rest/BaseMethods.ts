89457d8bbbdee4c8182f107733087f253b4ac6b0
Juan Laquiz <juanilaquiz@gmail.com>
2023-05-12 11:01:39 -0300

    Log request times in lib

--- src/rest/BaseMethods.ts
+++ src/rest/BaseMethods.ts
@@ -1,14 +1,17 @@
 import axios from 'axios';
-import { Headers } from '../types.js';
+import { withLogging } from '../decorators/WithLogging.js';
 import { withRetries } from '../decorators/WithRetries.js';
+import { Headers } from '../types.js';
 
 export const get = async <T>(
   url: string,
   headers: Headers,
   params?: Record<string, string | number | boolean | undefined>
 ): Promise<T> => {
-  console.log('⚡️Splight -  GET', url);
-  const { data } = await withRetries(axios<T>)(url, { headers, params });
+  const { data } = await withRetries(() => {
+    return withLogging('GET', url, axios<T>)(url, { headers, params });
+  })();
+
   return data;
 };
 
@@ -18,13 +21,19 @@ export const post = async <I, O>(
   headers: Headers,
   params?: Record<string, string | number | boolean | undefined>
 ): Promise<O> => {
-  console.log('⚡️Splight -  POST', url);
-  const { data: response } = await withRetries(axios<O>)(url, {
-    method: 'post',
-    data,
-    headers,
-    params,
-  });
+  const { data: response } = await withRetries(() => {
+    return withLogging(
+      'POST',
+      url,
+      axios<O>
+    )(url, {
+      method: 'post',
+      data,
+      headers,
+      params,
+    });
+  })();
+
   return response;
 };
 
@@ -33,29 +42,47 @@ export const patch = async <I, O>(
   data: I,
   headers: Headers
 ): Promise<O> => {
-  console.log('⚡️Splight -  PATCH', url);
-  const { data: response } = await withRetries(axios<O>)(url, {
-    method: 'patch',
-    data,
-    headers,
-  });
+  const { data: response } = await withRetries(() => {
+    return withLogging(
+      'PATCH',
+      url,
+      axios<O>
+    )(url, {
+      method: 'patch',
+      data,
+      headers,
+    });
+  })();
+
   return response;
 };
 
 export const del = async <T>(url: string, headers: Headers): Promise<T> => {
-  console.log('⚡️Splight -  DELETE', url);
-  const { data: response } = await withRetries(axios<T>)(url, {
-    method: 'delete',
-    headers,
-  });
+  const { data: response } = await withRetries(() => {
+    return withLogging(
+      'DELETE',
+      url,
+      axios<T>
+    )(url, {
+      method: 'delete',
+      headers,
+    });
+  })();
+
   return response;
 };
 
 export const options = async <T>(url: string, headers: Headers): Promise<T> => {
-  console.log('⚡️Splight -  OPTIONS', url);
-  const { data: response } = await withRetries(axios<T>)(url, {
-    method: 'options',
-    headers,
-  });
+  const { data: response } = await withRetries(() => {
+    return withLogging(
+      'OPTIONS',
+      url,
+      axios<T>
+    )(url, {
+      method: 'options',
+      headers,
+    });
+  })();
+
   return response;
 };
