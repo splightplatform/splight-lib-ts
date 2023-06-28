62b4b42b9494b5d61633aaa7e76cd7f7f42b1f4d
Victor Martini <victor.cesar.m@gmail.com>
2023-06-28 09:51:57 -0300

    Make limit and since optional

--- src/engine/components/Components.ts
+++ src/engine/components/Components.ts
@@ -233,7 +233,7 @@ export const ComponentsClient = (headers: Headers) => {
         basePath.slash(pk).slash('connections').url,
         headers
       ),
-    logs: (pk: string, params: { limit: number; since: string }) =>
+    logs: (pk: string, params: { limit?: number; since?: string }) =>
       get<string[]>(basePath.slash(pk).slash('logs').url, headers, params),
   };
 };
