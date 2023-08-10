b9f1234dd8a5e978e6898e2ea5cc18fc4eee9a83
NahuelBurdisso <nahuelburdisso@hotmail.com>
2023-08-10 15:00:26 -0300

    Add mock types

---
+++ src/constants/mockModes.ts
@@ -0,0 +1,8 @@
+import { MockMode } from '../types.js';
+
+export const MOCK_MODES: MockMode[] = [
+  {
+    key: 'fakeOrganization',
+    label: 'Fake Organization',
+  },
+];
---
+++ src/constants/tourModes.ts
@@ -0,0 +1,9 @@
+import { TourMode } from '../types.js';
+
+export const TOUR_MODES: TourMode[] = [
+  {
+    mockServerKey: 'modbus',
+    key: 'modbus',
+    label: 'MODBUS Tour',
+  },
+];
