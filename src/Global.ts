5e59817163b787ef3d726e6752c729527949bfb3
NahuelBurdisso <nahuelburdisso@hotmail.com>
2023-09-08 10:52:59 -0300

    Update version

--- src/Global.ts
+++ src/Global.ts
@@ -7,9 +7,7 @@ export let API_HOST =
 
 export const Global = {
   getAPIHost: () => API_HOST,
-  setAPIHost: (newAPIHost?: string) => {
-    if (newAPIHost !== undefined) {
-      API_HOST = newAPIHost ?? API_HOST;
-    }
+  setAPIHost: (newAPIHost: string) => {
+    API_HOST = newAPIHost;
   },
 };
