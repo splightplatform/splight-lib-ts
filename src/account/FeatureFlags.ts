ea6e1ebe4b21bfeb0da74a356a52de9f28e412be
Ignacio Fernandez <nachofernandez@Ignacios-MacBook-Pro.local>
2023-09-14 15:15:54 -0300

    changed endpoint to feature-flags

--- src/account/FeatureFlags.ts
+++ src/account/FeatureFlags.ts
@@ -3,7 +3,7 @@ import { Headers } from '../types.js';
 import { Path } from '../Urls.js';
 
 export const FeatureFlagsClient = (headers: Headers) => {
-  const basePath = Path('v2/account/feature_flags/');
+  const basePath = Path('v2/account/feature-flags/');
   const { list } = BaseRestClient<string[]>(
     basePath,
     headers
