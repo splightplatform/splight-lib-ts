bb81735cc52da947b309573b0f846763fcb3da94
Ignacio Fernandez <nachofernandez@Ignacios-MacBook-Pro.local>
2023-09-29 16:12:21 -0300

    deployment_active boolean

--- src/engine/components/Components.ts
+++ src/engine/components/Components.ts
@@ -173,7 +173,7 @@ export interface Component {
   deployment_capacity?: string;
   deployment_log_level?: number;
   deployment_status: string;
-  deployment_active: string;
+  deployment_active: boolean;
   deployment_restart_policy?: string;
   connections_active: boolean;
   connections_updated_at: string;
