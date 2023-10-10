84ac6a1c8dd89b759aaca50b25d1bb4b96c1dfd8
Matias Silva <matias.silva@splight-ae.com>
2023-10-10 13:37:03 -0300

    Backoffice updates

--- src/Backoffice.ts
+++ src/Backoffice.ts
@@ -11,6 +11,6 @@ export const Backoffice = (headers: Headers) => ({
   organizations: OrganizationsClient(headers),
   contracts: ContractsClient(headers),
   features: FeaturesClient(headers),
-  computeNode: ComputeNodesClient(headers),
+  computeNodes: ComputeNodesClient(headers),
   assets: AssetsClient(headers),
 });
