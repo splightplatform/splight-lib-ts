77aee53f3dfc03a53ca813b70b4ab7575b4cc5ab
Victor Martini <victor.cesar.m@gmail.com>
2023-11-13 15:08:52 -0300

    Add methods for consuming weather data

---
+++ src/engine/weather/Weather.ts
@@ -0,0 +1,63 @@
+import { get } from '../../rest/BaseMethods.js';
+import { Headers } from '../../types.js';
+import { Path } from '../../Urls.js';
+
+export interface CurrentWeather {
+  icon_code: number;
+  temperature: number;
+  phrase: string;
+  wind_speed: number;
+  wind_direction: number;
+  day_or_night: string;
+  sunset_time?: string;
+  sunrise_time?: string;
+}
+
+export interface DailyForecast {
+  day_of_week: string;
+  min_temperature: number;
+  max_temperature: number;
+  sunrise_time: string;
+  sunset_time: string;
+  narrative: string;
+  icon_code?: number;
+}
+
+export interface HourlyForecast {
+  temperature?: number;
+  icon_code?: number;
+  time?: string;
+}
+
+export const WeatherClient = (headers: Headers) => {
+  const basePath = Path('v2/engine/weather/');
+
+  return {
+    currentWeather: (latitude: number, longitude: number) =>
+      get<CurrentWeather>(
+        basePath
+          .slash('current')
+          .slash(latitude.toString())
+          .slash(longitude.toString()).url,
+        headers
+      ),
+    dailyForecast: (latitude: number, longitude: number) =>
+      get<DailyForecast[]>(
+        basePath
+          .slash('forecast')
+          .slash('daily')
+          .slash(latitude.toString())
+          .slash(longitude.toString()).url,
+        headers
+      ),
+    hourlyForecast: (latitude: number, longitude: number) =>
+      get<HourlyForecast[]>(
+        basePath
+          .slash('forecast')
+          .slash('hourly')
+          .slash(latitude.toString())
+          .slash(longitude.toString()).url,
+        headers
+      ),
+  };
+};
