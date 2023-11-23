fc1e2cc4777b885c22b74f4615249d4801c8ca5e
Matias Silva <matias.silva@splight-ae.com>
2023-11-23 09:44:41 -0300

    Exports utils as paart of the package

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import { Splight } from './Setup.js';
 import { Global } from './Global.js';
+import { Path } from './Urls.js';
+import { BaseRestClient, getHeaders } from './rest/BaseRestClient.js';
 
+export * from './rest/BaseMethods.js';
 export * from './types.js';
 export { Global };
+export { Path };
+export { BaseRestClient, getHeaders };
+
 export { Splight } from './Setup.js';
 
 export default Splight;
