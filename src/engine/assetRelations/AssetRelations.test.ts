8c57db49d7fd77e2ee1bb3f862e10f73eb3d879c
Federico Gonzalez Kriegel <federico.kriege@splight-ae.com>
2024-02-21 17:36:38 -0300

    Fix typo

--- src/engine/assetRelations/AssetRelations.test.ts
+++ src/engine/assetRelations/AssetRelations.test.ts
@@ -24,7 +24,7 @@ test('List asset relations', async () => {
     data: { results: [], next: 'something' },
     status: 200,
   });
-  await splight.engine.AssetRelations.list();
+  await splight.engine.assetRelations.list();
   expect(mockedAxios).toHaveBeenCalledWith(
     `${API_HOST}v2/engine/asset/relations/`,
     {
@@ -38,7 +38,7 @@ test('List asset relations with params', async () => {
     data: { results: [], next: 'something' },
     status: 200,
   });
-  await splight.engine.AssetRelations.list({
+  await splight.engine.assetRelations.list({
     page_size: 10,
   });
   expect(mockedAxios).toHaveBeenCalledWith(
@@ -55,7 +55,7 @@ test('Retrieve asset relations', async () => {
     data: MockAssetRelations,
     status: 200,
   });
-  await splight.engine.AssetRelations.retrieve('123');
+  await splight.engine.assetRelations.retrieve('123');
   expect(mockedAxios).toHaveBeenCalledWith(
     `${API_HOST}v2/engine/asset/relations/123/`,
     {
@@ -69,7 +69,7 @@ test('Create asset relations', async () => {
     data: MockAssetRelations,
     status: 201,
   });
-  await splight.engine.AssetRelations.create(MockAssetRelationsParam);
+  await splight.engine.assetRelations.create(MockAssetRelationsParam);
   expect(mockedAxios).toHaveBeenCalledWith(
     `${API_HOST}v2/engine/asset/relations/`,
     {
@@ -85,7 +85,7 @@ test('Update asset relations', async () => {
     data: { ...MockAssetRelations, name: 'updated' },
     status: 200,
   });
-  await splight.engine.AssetRelations.update('123', {
+  await splight.engine.assetRelations.update('123', {
     ...MockAssetRelationsParam,
     name: 'updated',
   });
@@ -103,7 +103,7 @@ test('Delete asset relations', async () => {
   mockedAxios.mockResolvedValueOnce({
     status: 204,
   });
-  await splight.engine.AssetRelations.destroy('123');
+  await splight.engine.assetRelations.destroy('123');
   expect(mockedAxios).toHaveBeenCalledWith(
     `${API_HOST}v2/engine/asset/relations/123/`,
     {
